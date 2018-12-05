9b5d3e4da2d069a27b230ce1403724d6115bfc17
Nikolai Hristov <hristov3@gmail.com>
2018-12-05 04:54:56 -0600

    Implementing Fortnite Activity's Nav Drawer

--- app/src/main/java/com/example/aceraspire/stattracker/FortniteActivity.java
+++ app/src/main/java/com/example/aceraspire/stattracker/FortniteActivity.java
@@ -1,5 +1,6 @@
 package com.example.aceraspire.stattracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class FortniteActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,21 +79,24 @@ public class FortniteActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        if (id == R.id.nav_overwatch) {
+            // Handle the overwatch tab action
+            // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new overwatch_frag()).commit();
+            startActivity(new Intent(getApplicationContext(),OWActivity.class));
+        } else if (id == R.id.nav_fortnite) {
+            // Handle the Fortnite Screens
+            // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fortnite_frag()).commit();
+            startActivity(new Intent(getApplicationContext(), FortniteActivity.class));
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            Toast.makeText(this, "REEEEEEEEEEE", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_send) {
-
+            Toast.makeText(this, "YEEEEEEEEEET", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
